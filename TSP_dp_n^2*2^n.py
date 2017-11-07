c3a08f01e71da9c657c883bd9e48b3c07aac281a
WenzhongLi <wenzhongli7@gmail.com>
2017-11-07 12:01:57 -0500

    init

---
+++ TSP_dp_n^2*2^n.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@author: li
+'''
+import sys
+import generator
+import itertools
+import copy
+
+class TSP(object):
+    def __init__(self):
+        return
+
+    def run_tsp(self, map, size):
+        result = 0
+        all_node = []
+        for n in range(size):
+            all_node.append(n)
+        path_for_all_start = dict()
+        min_path_for_all_start = dict()
+        for start in range(size):
+            removed_list = copy.deepcopy(all_node)
+            removed_list.remove(start)
+            path_for_current_start = dict()
+            path_for_current_start_two_element = dict()
+            path_for_current_start[2] = path_for_current_start_two_element
+            for j in range(size):
+                if j == start:
+                    continue
+                two_node_list = [start, j]
+                two_node_list_sorted = sorted(two_node_list)
+                path_for_current_start_two_element[(start, tuple(two_node_list_sorted), j)] = two_node_list
+            for set_size in range(3, size+1):
+                path_for_current_start_num_of_element = dict()
+                path_for_current_start[set_size] = path_for_current_start_num_of_element
+                all_node_list_with_out_count_current_start = copy.copy(all_node)
+                all_node_list_with_out_count_current_start.remove(start)
+                for subset in itertools.combinations(all_node_list_with_out_count_current_start, set_size-1):
+                    set_list = list(subset)
+                    set_list.append(start)
+                    set_list = sorted(set_list)
+                    for current_end in subset:
+                        if current_end == start:
+                            print subset, current_end
+                            continue
+                        set_list_with_out_current_end = copy.copy(set_list)
+                        set_list_with_out_current_end.remove(current_end)
+                        path_for_last_start_num_of_element = path_for_current_start[set_size-1]
+                        # (start,subset,end)
+                        min_cost = -1
+                        min_path = []
+                        for last_internode in set_list_with_out_current_end:
+                            if last_internode == start:
+                                #print set_list_with_out_current_end, last_internode
+                                continue
+                            mid_list = copy.deepcopy(set_list)
+                            mid_list.remove(current_end)
+                            #mid_list.insert(0,start)
+                            if (start,tuple(mid_list),last_internode) in path_for_last_start_num_of_element:
+                                mid_path = path_for_last_start_num_of_element[(start, tuple(mid_list), last_internode)]
+                                current_cost = self.countcost(map, mid_path)
+                                if min_cost == -1 or min_cost > current_cost + map[last_internode][current_end]:
+                                    min_cost = current_cost + map[last_internode][current_end]
+                                    min_path = copy.deepcopy(mid_path)
+                                    min_path.append(current_end)
+                        if min_cost != -1:
+                            current_size_path = path_for_current_start[set_size]
+                            current_size_path[(start, tuple(sorted(min_path)), current_end)] = min_path
+            path_for_all_start[start] = path_for_current_start
+            min_path_for_current_start = []
+            min_length_for_current_start = -1
+            last_step_map = path_for_current_start[size]
+            for path in last_step_map:
+                length_of_current = self.countcost(map,last_step_map[path])
+                if min_length_for_current_start == -1 \
+                        or min_length_for_current_start > length_of_current + map[path[0]][path[2]]:
+                    min_length_for_current_start = length_of_current + map[path[0]][path[2]]
+                    min_path_for_current_start = copy.copy(last_step_map[path])
+                    min_path_for_current_start.append(start)
+            if min_length_for_current_start != -1:
+                min_path_for_all_start[start] = (min_length_for_current_start, min_path_for_current_start)
+                self.countcost(map,min_path_for_current_start)
+                print min_path_for_current_start
+            else:
+                print "error"
+
+        print min_path_for_all_start
+        return result
+
+    def countcost(self, map, path):
+        last = -1
+        cost = 0
+        for node in path:
+            if last == -1:
+                last = node
+            else:
+                cost += map[node][last]
+                last = node
+        return cost
+
+
+
+if __name__ == "__main__":
+    print "script_name", sys.argv[0]
+    for i in range(1, len(sys.argv)):
+        print "argment", i, sys.argv[i]
+    print ('start initialize')
+    tsp = TSP()
+    size = 10
+    max_length = 100
+    generator = generator.Generator(size, max_length)
+    generator.paint_random()
+    generator.print_matrix()
+    r = tsp.run_tsp(generator.get_matrix(), size)
+    print r
---
+++ generator.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@author: li
+'''
+import random
+import sys
+
+
+class Generator(object):
+    # init size and density of map
+    def __init__(self, size, max_length):
+        self.size = size
+        self.max_length = max_length
+        self.map_matrix = []
+        for k in range(self.size):
+            self.map_matrix.append([])
+            for j in range(self.size):
+                self.map_matrix[k].append(0)
+
+    # print map to commend line
+    def print_matrix(self):
+        count_blocked = 0
+        for k in range(self.size + 1):
+            for j in range(self.size + 1):
+                if k == 0 and j == 0:
+                    print "  ",
+                    continue
+                elif k == 0:
+                    print " " + str(j - 1),
+                    continue
+                elif j == 0:
+                    print " " + str(k - 1),
+                    continue
+                if self.map_matrix[k-1][j-1] < 10:
+                    print " "+str(self.map_matrix[k-1][j-1]),
+                elif self.map_matrix[k-1][j-1] < 100:
+                    print self.map_matrix[k-1][j-1],
+                else:
+                    print(self.map_matrix[k-1][j-1]),
+            print('\n'),
+
+    # paint maze randomly
+    def paint_random(self):
+        matrix = [[0 for j in range(self.size)] for k in range(self.size)]
+        node_list = []
+        # init a set of all point could be block
+        for k in range(self.size):
+            for j in range(self.size):
+                matrix[k][j] = random.choice(range(self.max_length))
+        for k in range(self.size):
+            for j in range(self.size):
+                matrix[k][j] = matrix[j][k]
+                if k == j:
+                    matrix[k][j] = 0
+        self.map_matrix = matrix
+        return matrix
+
+    def get_matrix(self):
+        return self.map_matrix
+
+
+if __name__ == "__main__":
+    print "script_name", sys.argv[0]
+    for i in range(1, len(sys.argv)):
+        print "argment", i, sys.argv[i]
+    print ('start initialize')
+    # set the size and density of this matrix
+    generator = Generator(10, 100)
+    generator.print_matrix()
+    generator.paint_random()
+    generator.print_matrix()
+    print ('start over')
